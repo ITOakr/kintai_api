4ce201427f1a79ffe0551d91355916ae560823e1
ak_r <ak_r@git.com>
2025-09-17 19:35:11 +0900

    前回のマイグレーションがrenderで失敗したのでカラムの追加から削除の順に修正

--- db/migrate/20250917052325_remove_is_active_and_add_status_to_users.rb
+++ db/migrate/20250917052325_remove_is_active_and_add_status_to_users.rb
@@ -1,13 +1,7 @@
 class RemoveIsActiveAndAddStatusToUsers < ActiveRecord::Migration[8.0]
   def change
-    # is_activeカラムを削除
-    if column_exists?(:users, :is_active)
-      remove_column :users, :is_active, :boolean
-    end
+    add_column :users, :status, :integer, default: 0, null: false
 
-    # statusカラムを追加 (0: pending, 1: active, 2: deleted)
-    unless column_exists?(:users, :status)
-      add_column :users, :status, :integer, default: 0, null: false
-    end
+    remove_column :users, :is_active, :boolean
   end
 end
