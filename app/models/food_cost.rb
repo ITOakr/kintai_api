cbf04ae2c75768a07d8ab8cb3e018882e4fb7748
ak_r <ak_r@git.com>
2025-09-19 15:23:58 +0900

    enumの文法に誤りがあったので修正

--- app/models/food_cost.rb
+++ app/models/food_cost.rb
@@ -1,10 +1,5 @@
 class FoodCost < ApplicationRecord
-  enum category: {
-    meat: 0,
-    ingredient: 1,
-    drink: 2,
-    other: 3
-  }
+  enum :category, { meat: 0, ingredient: 1, drink: 2, other: 3 }
 
   validates :date, presence: true
   validates :amount_yen, numericality: { greater_than_or_equal_to: 0, only_integer: true }
